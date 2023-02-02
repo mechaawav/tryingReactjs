44f15587139e862775da4ffc3234c7892e086f34
Mecha <mechi13carabajal@gmail.com>
2023-02-02 16:49:18 -0300

    modifique archivos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+
+let datos = {
+  nombre: 'Raul',
+  apellido: 'Lopez',
+  edad: 42
+}
+
+const {nombre, edad} = datos
+
+const evento = () => {
+  console.log('hola mundo')
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>El usuario se llama {nombre} y su edad es {edad}</h1>
+       <button onClick={evento}>Hace Click!</button>
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import './Header.css'
+
+const Header = () => {
+    return(
+    <header>
+      <h1 className='title'>MERCEDES</h1>
+      <nav>
+        <a> INICIO </a>
+        <a> ACERCA </a>
+        <a> SERVICIOS </a>
+        <a> RESUME </a>
+        <a> PORTFOLIO </a>
+        <a> CONTACTO </a>
+       </nav>
+
+    </header>
+    )
+
+}
+
+export default Header
